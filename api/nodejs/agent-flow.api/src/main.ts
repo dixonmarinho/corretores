0def9d35204998849719063caa4a6cc04d1a0534
Ney Dixon Santiago Marinho <dixonsm@gmail.com>
2026-01-23 22:08:48 -0300

    Changes

--- api/nodejs/agent-flow.api/src/main.ts
+++ api/nodejs/agent-flow.api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// 1. Importe estas duas linhas
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 2. Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Agent Flow API') // Nome do seu projeto
+    .setDescription('Documentação da API do Agent Flow')
+    .setVersion('1.0')
+    .addTag('users') // Opcional: tags iniciais
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // A rota será: http://localhost:3000/api
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
